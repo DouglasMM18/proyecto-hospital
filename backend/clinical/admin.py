4d566adc6270e60832a4105454d1a5ac2248b05f
Douglas <douglasmezamorales@gmail.com>
2025-12-08 15:02:49 -0300

    Admin panel configurado: Selector de Roles para usuarios

--- backend/clinical/admin.py
+++ backend/clinical/admin.py
@@ -1,18 +1,32 @@
 from django.contrib import admin
-from .models import Madre, Parto, RecienNacido
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Madre, Parto, RecienNacido, PerfilUsuario
+
+# --- CONFIGURACIÓN DE ROLES ---
+
+class PerfilUsuarioInline(admin.StackedInline):
+    model = PerfilUsuario
+    can_delete = False
+    verbose_name_plural = 'Rol Asignado'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (PerfilUsuarioInline,)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+# --- MODELOS CLÍNICOS ---
 
 @admin.register(Madre)
 class MadreAdmin(admin.ModelAdmin):
-    # Mostramos columnas clave. Recuerda que RUT y Nombre se descifran solos al leerse.
     list_display = ('id', 'rut', 'nombre_completo', 'fecha_nacimiento', 'comuna')
-    # Buscador: Django intentará buscar en texto cifrado, lo cual es complejo. 
-    # Para el MVP, la búsqueda en campos cifrados es limitada, pero lo configuramos igual.
     search_fields = ('comuna',) 
 
 @admin.register(Parto)
 class PartoAdmin(admin.ModelAdmin):
     list_display = ('id', 'fecha', 'hora', 'tipo_parto', 'profesional_acargo', 'madre')
-    list_filter = ('tipo_parto', 'fecha') # Filtros laterales útiles
+    list_filter = ('tipo_parto', 'fecha')
 
 @admin.register(RecienNacido)
 class RecienNacidoAdmin(admin.ModelAdmin):
